16d6d4cc3023b4a079b121c3f070850f73721c8e
Clint Jackson <clint.jackson09@gmail.com>
2024-03-18 07:39:07 -0500

    css, little learning checks/test

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,9 +1,12 @@
 import satData from './satData.jsx';
 import { useState } from 'react';
-
+import styles from "./styles.module.css"
+//the css styling was working before I even imported that...not sure why. 
 const Buttons = ({ filterByType, setSat, displaySats }) => {
   return(
-  <div>
+  <div className="flex-container">
+    <button onClick = {()=>setSat(satData)}> All Orbits </button>
+    {/* sets sat to full satData*/}
   {displaySats.map((sat, id)=> (
     //mapping over displaySats, which is the 3 orbit types only, no repeats. 
     //filterByType is called on click, with the argument "sat" the value of which is stored by the button itself. 
@@ -12,8 +15,7 @@ const Buttons = ({ filterByType, setSat, displaySats }) => {
       </button>    
     ))}
 
-    <button onClick = {()=>setSat(satData)}> All Orbit </button>
-    {/* sets sat to full satData*/}
+    
   </div>  
   );
 };
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,5 +1,6 @@
 import satData from "./satData";
 import App from "../App.jsx";
+import styles from "./styles.module.css"
 //map over current value of sat. sat's value is modified by buttons. ergo, it only receives what sat has, and so only has that to map over and display. 
 //I tried removing medium to see what happens if the medium button is clicked, but the medium button disappeared lol cuz that's mapped too. The set on app.jsx never received that value to include, and the map on button.jsx never received it to create a button with it.  
 const Table = ({ sat }) => {
